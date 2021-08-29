1ea302bd3b2d34a1defc29df92566a14ede2c897
hyv1001 <hyv1001@163.com>
2021-08-30 00:24:11 +0800

    Implement MVP, Coordinate and Camea System

---
+++ src/camera.h
@@ -0,0 +1,27 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Camera
+{
+public:
+	static const glm::vec3 UP_DIR;
+
+	Camera() { }
+	Camera(const glm::vec3& camera_pos, const glm::vec3& target_pos) :
+		camera_position_(camera_pos), target_position_(target_pos) {}
+
+	void setPosition(const glm::vec3& pos) { camera_position_ = pos; }
+	void setTarget(const glm::vec3& target_pos) { target_position_ = target_pos; }
+
+	glm::vec3 getPosition() const { return camera_position_; }
+	glm::vec3 getDirection() const { return glm::normalize(target_position_ - camera_position_); }
+	glm::mat4 getLookAt() const { return glm::lookAt(camera_position_, target_position_, UP_DIR); }
+
+
+protected:
+	glm::vec3 camera_position_{ 0.f, 0.f, 0.f };
+	glm::vec3 target_position_{ 0.f, 0.f, 0.f };
+};
+
+const glm::vec3 Camera::UP_DIR = glm::vec3(0.f, 1.f, 0.f);
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 
 #include "shader.h"
+#include "camera.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image/stb_image.h>
@@ -14,15 +15,64 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
-const int k_width = 1024;
-const int k_height = 1024;
+const int k_width = 800;
+const int k_height = 600;
 
-const float vertices[] = {
-	// position                 // color                // texture coords
-	0.5f,   0.5f,   0.0f,       1.0f,   0.0f,   0.0f,   1.0f, 1.0f,
-	0.5f,   -0.5f,  0.0f,       0.0f,   1.0f,   0.0f,   1.0f, 0.0f,
-	-0.5f,  -0.5f,  0.0f,       0.0f,   0.0f,   1.0f,   0.0f, 0.0f,
-	-0.5f,  0.5f,  0.0f,        1.0f,   1.0f,   0.0f,   0.0f, 1.0f
+float vertices[] = {
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
+
+const glm::vec3 cubePositions[] = {
+	glm::vec3(0.0f,  0.0f,  0.0f),
+	glm::vec3(2.0f,  5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f,  3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f)
 };
 
 const uint32_t indices[] = {
@@ -56,6 +106,8 @@ uint32_t createTexture(const char* texture_file, GLenum format)
 	return texture;
 }
 
+Camera camera(glm::vec3(0.f, 0.f, 3.f), glm::vec3(0.f, 0.f, 0.f));
+
 int main()
 {
 	if (!glfwInit())
@@ -83,6 +135,8 @@ int main()
 		return -1;
 	}
 
+	glEnable(GL_DEPTH_TEST);
+	//glDepthFunc(GL_ALWAYS);
 	glViewport(0, 0, k_width, k_height);
 
 	stbi_set_flip_vertically_on_load(true);
@@ -92,10 +146,9 @@ int main()
 
 	Shader shader("E:\\projects\\learn_opengl\\shader\\shader.vs", "E:\\projects\\learn_opengl\\shader\\shader.fs");
 
-	uint32_t VAO, VBO, EBO;
+	uint32_t VAO, VBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
 
 	// 1. bind Vertex Array Object
 	glBindVertexArray(VAO);
@@ -103,44 +156,45 @@ int main()
 	// 2. copy our vertices array in a buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	// 3. set the vertex attributes pointers
 	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (const void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (const void*)0);
 	glEnableVertexAttribArray(0);
 	// color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (const void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
+	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 36 * sizeof(float), (const void*)(3 * sizeof(float)));
+	//glEnableVertexAttribArray(1);
 	// texture attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (const void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (const void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// Note that this is allowed, the call to glVertexAttribPointer registeed VBO as the
 	// veretx attribute's bound vertex buffer object so afterwards we can safely unbind
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	// You can unbind the VAO afterwards so other VAO calls won't accidentally modiy this 
 	// VAO, but this rarely happens. Modifying other VAOs requires to glBindVertexArray 
 	// anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly neccessary.
-	glBindVertexArray(0);
+	//glBindVertexArray(0);
 
-	glm::mat4 trans = glm::mat4(1.f);
 
 	shader.use();
 	shader.setInt("texture1", 0);
 	shader.setInt("texture2", 1);
-	shader.setMat4fv("transform", glm::value_ptr(trans));
+
 
 	//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
+	glm::mat4 view = camera.getLookAt();
+	glm::mat4 projection = glm::mat4(1.f);
+	projection = glm::perspective(glm::radians(45.f), (float)k_width / (float)k_height, 0.1f, 100.f);
 
+	uint32_t frame_index = 0;
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture_1);
@@ -148,29 +202,37 @@ int main()
 		glBindTexture(GL_TEXTURE_2D, texture_2);
 
 		shader.use();
-		trans = glm::mat4(1.f);
-		trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.f, 0.f, 1.f));
-		shader.setMat4fv("transform", glm::value_ptr(trans));
+
+		glm::mat4 model = glm::mat4(1.f);
+		model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.f, 0.f));
+		view = camera.getLookAt();
+
+		shader.setMat4fv("model", glm::value_ptr(model));
+		shader.setMat4fv("view", &view[0][0]);
+		shader.setMat4fv("projection", glm::value_ptr(projection));
 
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-		trans = glm::mat4(1.f);
-		trans = glm::translate(trans, glm::vec3(-0.5f, 0.5f, 0.0f));
-		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.f, 0.f, 1.f));
-		shader.setMat4fv("transform", glm::value_ptr(trans));
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		for (int index = 0; index < 10; index++)
+		{
+			model = glm::mat4(1.f);
+			model = glm::translate(model, cubePositions[index]);
+			float angle = 20.f * index;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.f, 0.3f, 0.5f));
+			shader.setMat4fv("model", glm::value_ptr(model));
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
+		frame_index++;
 	}
 
 	// de-allocate all resources once they've outlived their purpose
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
+	//glDeleteBuffers(1, &EBO);
 	//glDeleteProgram(shaderProgram1);
 	//glDeleteProgram(shaderProgram2);
 
@@ -188,4 +250,26 @@ void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	const float camear_speed = 0.05f;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		camera.setPosition(camera.getPosition() + camear_speed * camera.getDirection());
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		camera.setPosition(camera.getPosition() - camear_speed * camera.getDirection());
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		camera.setPosition(camera.getPosition() - camear_speed * (glm::normalize(glm::cross(camera.getDirection(), Camera::UP_DIR))));
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		camera.setPosition(camera.getPosition() + camear_speed * (glm::normalize(glm::cross(camera.getDirection(), Camera::UP_DIR))));
+	}
+
 }
